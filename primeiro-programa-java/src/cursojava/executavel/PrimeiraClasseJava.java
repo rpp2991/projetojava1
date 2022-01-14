9d77eb240726f6e81c294671b3e1a9e15adc4df2
Windows <Windows@DESKTOP-HADHDK8>
2022-01-14 19:50:34 -0300

    Criação do Projeto

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,10 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+
+	/*Main é um método auto executável em Java*/
+	public static void main(String[] args) {
+		
+		System.out.println("Meu primeiro programa em JAVA - do curso Formação Java Web");
+	}
+}
